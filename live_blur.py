9520982342cfb2203a84031d8403d429ffed0450
henrick.nogueira <ricknogueira1231@gmail.com>
2025-08-30 18:43:00 -0300

    Função geradora de Kernels

--- live_blur.py
+++ live_blur.py
@@ -4,7 +4,19 @@ import scipy
 import scipy.ndimage
 from scipy import signal
 
+PATH_XML = 'haarcascade_frontalface_default.xml'
+
+def generate_kernel(kernel_len=5, desvio_padrao=5):
+    generate_kernel1d = signal.gaussian(kernel_len, std=desvio_padrao).reshape(kernel_len, 1)
+    generate_kernel2d = np.outer(generate_kernel1d, generate_kernel1d)
+
+    return generate_kernel2d
+
+
 video_capture = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascade + PATH_XML)
+
+kernel = generate_kernel()
 
 while True:
     ret, frame = video_capture.read()
